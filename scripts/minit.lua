394558a8a8e284af20928994fb62108093e8d5ed
Simon Ho <simonho.ubc@gmail.com>
2025-03-03 01:08:53 -0800

    ci(tests): install luarocks
    ci(tests): install lua

--- scripts/minit.lua
+++ scripts/minit.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S nvim -l
 
-vim.env.LAZY_STDPATH = ".extras"
+vim.env.LAZY_STDPATH = ".tests"
 load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
 
 -- Setup lazy
