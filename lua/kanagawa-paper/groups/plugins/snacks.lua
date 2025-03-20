4f0dbbfd9494408c3518f52da61fe8284a6eb90d
Simon Ho <simonho.ubc@gmail.com>
2025-03-19 23:53:26 -0700

    fix(snacks): more consistent cursorline colors for pickers and input

--- lua/kanagawa-paper/groups/plugins/snacks.lua
+++ lua/kanagawa-paper/groups/plugins/snacks.lua
@@ -7,6 +7,9 @@ function M.get(colors, opts)
 		SnacksIndent = { fg = theme.ui.indent },
 		SnacksIndentScope = { fg = theme.ui.indent_scope },
 		SnacksIndentChunk = { fg = theme.ui.indent_scope },
+		SnacksPickerCursorLine = { bg = theme.ui.float.bg },
+		SnacksPickerListCursorLine = { bg = theme.ui.bg_cursorline_alt },
+		SnacksPickerPreviewCursorLine = { bg = theme.ui.bg_cursorline_alt },
 	}
 end
 
