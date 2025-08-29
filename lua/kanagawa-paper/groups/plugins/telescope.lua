643aad0ab04f67e3d4e366f27be3116d752496fd
Simon Ho <5199715+thesimonho@users.noreply.github.com>
2025-08-29 13:45:24 -0700

    fix(telescope): match telescope colors to other pickers (#64)

--- lua/kanagawa-paper/groups/plugins/telescope.lua
+++ lua/kanagawa-paper/groups/plugins/telescope.lua
@@ -4,23 +4,28 @@ function M.get(colors, opts)
 	local theme = colors.theme
 
 	return {
-		TelescopeTitle = { fg = theme.ui.fg_dark, bg = theme.accent.accent1, bold = true },
-		TelescopeBorder = { fg = theme.ui.float.fg_border, bg = theme.ui.bg },
+		TelescopeTitle = { bg = theme.accent.accent1, bold = true },
+		TelescopeBorder = { link = "FloatBorder" },
+
 		TelescopeSelection = { link = "CursorLineAlt" },
 		TelescopeSelectionCaret = { link = "CursorLineNr" },
-		TelescopePromptNormal = { bg = theme.ui.bg_p2 },
-		TelescopePromptBorder = { fg = theme.ui.bg_p2, bg = theme.ui.bg_p2 },
+
+		TelescopePromptNormal = { link = "NormalFloat" },
+		TelescopePromptBorder = { link = "FloatBorder" },
+		TelescopePromptPrefix = { link = "CursorLineNr" },
+
+		TelescopeResultsNormal = { link = "NormalFloat" },
+		TelescopeResultsBorder = { link = "FloatBorder" },
 		TelescopeResultsClass = { link = "Structure" },
 		TelescopeResultsStruct = { link = "Structure" },
 		TelescopeResultsField = { link = "@field" },
 		TelescopeResultsMethod = { link = "Function" },
 		TelescopeResultsVariable = { link = "@variable" },
-		TelescopeResultsTitle = { fg = theme.ui.fg_dark, bg = theme.accent.accent2, bold = true },
-		TelescopeResultsNormal = { fg = theme.ui.fg, bg = theme.ui.bg_p1 },
-		TelescopeResultsBorder = { fg = theme.ui.bg_p2, bg = theme.ui.bg_p1 },
-		TelescopePreviewTitle = { fg = theme.ui.fg_dark, bg = theme.accent.accent3, bold = true },
-		TelescopePreviewNormal = { bg = theme.ui.bg },
-		TelescopePreviewBorder = { bg = theme.ui.bg, fg = theme.ui.bg_p2 },
+		TelescopeResultsTitle = { bg = theme.accent.accent2, bold = true },
+
+		TelescopePreviewTitle = { bg = theme.accent.accent3, bold = true },
+		TelescopePreviewNormal = { link = "NormalFloat" },
+		TelescopePreviewBorder = { link = "FloatBorder" },
 	}
 end
 
