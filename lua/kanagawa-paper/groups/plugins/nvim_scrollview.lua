6791df8892f22e5c74441106cb75d90105e2da00
Simon Ho <simonho.ubc@gmail.com>
2025-09-14 17:54:52 -0700

    feat(scrollview): add colors for nvim-scrollview

---
+++ lua/kanagawa-paper/groups/plugins/nvim_scrollview.lua
@@ -0,0 +1,24 @@
+local color = require("kanagawa-paper.lib.color")
+local M = {}
+
+function M.get(colors, opts)
+	local theme = colors.theme
+
+	return {
+		ScrollView = { bg = theme.ui.scrollbar },
+		ScrollViewHover = { bg = color(theme.ui.scrollbar):brighten(0.1):to_hex() },
+
+		ScrollViewCursor = { fg = theme.ui.fg },
+		ScrollViewMarks = { fg = theme.ui.mark },
+		ScrollViewFolds = { fg = theme.ui.nontext },
+		ScrollViewSearch = { fg = theme.ui.bg_search },
+
+		ScrollViewKeywordsFix = { link = "DiagnosticSignError" },
+		ScrollViewKeywordsHack = { link = "DiagnosticSignWarn" },
+		ScrollViewKeywordsTodo = { link = "DiagnosticSignHint" },
+		ScrollViewKeywordsWarn = { link = "DiagnosticSignWarn" },
+		ScrollViewKeywordsXxx = { link = "DiagnosticSignInfo" },
+	}
+end
+
+return M
