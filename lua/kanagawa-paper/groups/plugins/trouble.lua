394980a2c0d9bac4a09cd00828a06cef3ce51588
Simon Ho <simonho.ubc@gmail.com>
2025-10-05 12:50:21 -0700

    fix(trouble): fix NC background color

--- lua/kanagawa-paper/groups/plugins/trouble.lua
+++ lua/kanagawa-paper/groups/plugins/trouble.lua
@@ -4,6 +4,8 @@ function M.get(colors, opts)
 	local theme = colors.theme
 
 	return {
+		TroubleNormal = { link = "Normal" },
+		TroubleNormalNC = { link = "NormalNC" },
 		TroubleIndent = { fg = theme.ui.indent },
 		TroublePos = { fg = theme.ui.special },
 		TroubleCount = { fg = theme.syn.special2 },
